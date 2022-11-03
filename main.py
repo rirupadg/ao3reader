5edb34be8817989eba7a89c1d1ed1354b531620d
clap nonyabusnis <>
2022-11-03 13:33:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from pywebcopy import save_webpage
+ 
+kwargs = {'project_name': 'site folder'}
+#page = '1'
+
+#import os
+#os.remove("https://replit.com/@clapnonyabusnis/ao3bb#this/ok/demofile.txt") 
+
+
+
+save_webpage(
+   
+    # url of the website
+    url='https://archiveofourown.org/tags/%E5%83%95%E3%81%AE%E3%83%92%E3%83%BC%E3%83%AD%E3%83%BC%E3%82%A2%E3%82%AB%E3%83%87%E3%83%9F%E3%82%A2%20%7C%20Boku%20no%20Hero%20Academia%20%7C%20My%20Hero%20Academia/works?page=3',
+     
+    # folder where the copy will be saved
+    project_folder='https://replit.com',
+    **kwargs
+)
+
+#f = open("https:/replit.com/site folder/archiveofourown.org/tags/Boku_no_Hero_Academia___My_Hero_Academia/works_page_2.html")
+#contents = f.read()
+#f.close()
+#print(contents)
+
+#file_obj = open("file_name.txt", "w") works_page_2.html
+                                  
+#print(user_input, file=file_obj)
+
+#file_obj.close()
---
+++ next.py
@@ -0,0 +1,3 @@
+import webbrowser
+url = 'https://replit.com/@clapnonyabusnis/ao3bb#https:/replit.com/site%20folder/archiveofourown.org/tags/Boku_no_Hero_Academia___My_Hero_Academia/works_page_3.html'
+webbrowser.open(url, new=2)  # open in new tab
